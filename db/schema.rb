84713d71e2f2aba75414fac56ebcdd417cd8681b
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-04-27 02:21:43 +0600

    schema?

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_426_185_738) do
+ActiveRecord::Schema.define(version: 20160426185738) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'answers', force: :cascade do |t|
-    t.text     'body'
-    t.integer  'question_id'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index 'answers', ['question_id'], name: 'index_answers_on_question_id', using: :btree
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
-  create_table 'questions', force: :cascade do |t|
-    t.string   'title'
-    t.text     'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
