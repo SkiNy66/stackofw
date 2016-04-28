667485d58a003aceebed32ed0c495f3d5cc650fd
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-04-28 17:51:20 +0600

    4 lesson video

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :answer do
-    body 'MyText'
+    body 'Answer_for_question'
     question_id 1
+  end
+  
+  factory :invalid_question, class: "Question" do
+    title nil
+    body nil
   end
 end
