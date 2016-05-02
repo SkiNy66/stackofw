75895140b774f33f79bd960d02c64afae3a64714
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-05-02 17:30:00 +0600

    finish new user acceptance

--- spec/acceptance/new_user_spec.rb
+++ spec/acceptance/new_user_spec.rb
@@ -6,6 +6,14 @@ feature 'Create new user', %q{
   I want to be able to register
 } do
   
-  scenario 'Guest crate new user'
-  scenario 'Autenticated user tries to create new user???????????'
+  scenario 'Guest crate new user' do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'Email', with: 'testreqister@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
 end
