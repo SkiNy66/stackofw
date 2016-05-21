e86b1e64ad8d66b00614be9ba21e5d7b1767523f
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-05-21 13:31:43 +0600

    finish lesson video

--- spec/acceptance/add_file_to_answer_spec.rb
+++ spec/acceptance/add_file_to_answer_spec.rb
@@ -14,11 +14,11 @@ feature 'Add files to answer', %q{
     visit question_path(question)
   end
 
-  scenario 'User adds file when asks answer' do
+  scenario 'User adds file when asks answer', js: true do
     fill_in 'body-for-new-answer', with: 'This is the answer'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Create answer'
-
+    
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
     end
