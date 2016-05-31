f7afa98976fd1ae92a5cc5437ccab24e9b896fa8
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-05-31 17:40:10 +0600

    add model comments

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  it { should belong_to :commentable }
+  it { is_expected.to validate_presence_of :body }
+  it { is_expected.to validate_presence_of :commentable_type }
+  it { is_expected.to validate_presence_of :commentable_id }
+end
