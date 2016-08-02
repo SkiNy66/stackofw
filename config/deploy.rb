4a7c75eb912f74b092223d3ca52d3dcf9184e5f2
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-08-03 00:42:48 +0600

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,3 +48,38 @@ namespace :deploy do
 
   after :publishing, :restart
 end
+
+namespace :private_pub do
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml start"
+        end
+      end
+    end
+  end
+
+  desc 'Stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml stop"
+        end
+      end
+    end
+  end
+
+  desc 'Restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
+end
