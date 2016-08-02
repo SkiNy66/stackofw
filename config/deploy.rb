2b8b49f0ae4471e12b3c0be59851e922ee04699e
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-08-03 00:27:12 +0600

    add private_pub settings to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_user, 'deployer'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/private_pub.yml', '.env'
+append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/private_pub_thin.yml', '.env'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
