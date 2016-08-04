f96ebcc38e0d5674700e412fe60f4e56a148379b
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-08-04 14:59:20 +0600

    delete as::author from folder indices

--- app/indices/answers_index.rb
+++ app/indices/answers_index.rb
@@ -1,7 +1,7 @@
 ThinkingSphinx::Index.define :answer, with: :active_record do
   #fields
   indexes body
-  indexes user.email, as: :author, sortable: true
+  indexes user.email, sortable: true
 
   #attributes
   has user_id, created_at, updated_at
