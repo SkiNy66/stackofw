94eb9897f0721820b56b3a8e67cd03052fcdfb69
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-08-06 01:35:29 +0600

    try thinkingsphinx fix 19

--- app/indices/comments_index.rb
+++ app/indices/comments_index.rb
@@ -1,8 +1,8 @@
-ThinkingSphinx::Index.define :comment, with: :active_record do
-  #fields
-  indexes body
-  indexes user.email, sortable: true
+# ThinkingSphinx::Index.define :comment, with: :active_record do
+#   #fields
+#   indexes body
+#   indexes user.email, sortable: true
 
-  #attributes
-  has user_id, created_at, updated_at
-end
+#   #attributes
+#   has user_id, created_at, updated_at
+# end
