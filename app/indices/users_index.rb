79e3ee1c4e50aa36d9277a894776919f9ef22136
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-08-08 00:53:09 +0600

    Revert "try thinkingsphinx fix 12"
    This reverts commit edfbc4117321acccfde3e4de1ea5d7d2619a90fe.

--- app/indices/users_index.rb
+++ app/indices/users_index.rb
@@ -1,7 +1,7 @@
-# ThinkingSphinx::Index.define :user, with: :active_record do
-#   #fields
-#   indexes email
+ThinkingSphinx::Index.define :user, with: :active_record do
+  #fields
+  indexes email
 
-#   #attributes
-#   has created_at
-# end
+  #attributes
+  has created_at
+end
