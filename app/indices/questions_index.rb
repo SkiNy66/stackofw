21ecfe4b54c08c339b25f35f451c72b0d8cecfb5
Timchenko Ilya <skiny66.ilya@gmail.com>
2016-08-08 00:53:08 +0600

    Revert "try thinkingsphinx fix 18"
    This reverts commit 888baa6b289c42502eb1c978f0729ac9b5dd5a21.

--- app/indices/questions_index.rb
+++ app/indices/questions_index.rb
@@ -1,9 +1,16 @@
-ThinkingSphinx::Index.define :question, with: :active_record do
-  #fields
-  indexes title, sortable: true
-  indexes body
-  indexes user.email, sortable: true
+# ThinkingSphinx::Index.define :question, with: :active_record do
+#   #fields
+#   indexes title, sortable: true
+#   indexes body
+#   indexes user.email, sortable: true
 
-  #attributes
+#   #attributes
+#   has user_id, created_at, updated_at
+# end
+
+ThinkingSphinx::Index.define :question, with: :active_record do
+  indexes title, :sortable => true
+  indexes body
+
   has user_id, created_at, updated_at
 end
